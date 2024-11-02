3f6f5cb25578be03cc49c74829b0fa144d1e0f25
Gagan Suman <fbdgagansuman@gmail.com>
2024-11-02 17:07:01 +0530

    Added Suspense

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="h-20 w-20 bg-red-400 rounded-s-lg rotate-30 animate-spin animate-pulse"></div>
+  );
+};
+
+export default Loading;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Suspense } from "react";
 
 import ShopContextProvider from "./Context/ShopContext.jsx";
+import Loading from "./components/Loading.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <BrowserRouter>
     <ShopContextProvider>
-      <App />
+      <Suspense fallback={<Loading/>}> 
+        <App />
+      </Suspense>
     </ShopContextProvider>
   </BrowserRouter>
   /* </StrictMode> */
